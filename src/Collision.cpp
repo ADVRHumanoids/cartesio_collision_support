d215037a91ec10ab4f7684c281910c0e36272c07
Arturo Laurenzi <arturo.laurenzi@iit.it>
2021-03-12 11:09:42 +0100

    minimalistic collision task and constraint integration

---
+++ src/Collision.cpp
@@ -0,0 +1,157 @@
+#include "Collision.h"
+#include <boost/make_shared.hpp>
+
+using namespace XBot::Cartesian;
+using namespace XBot::Cartesian::collision;
+
+const int default_max_pairs = 15;
+
+namespace
+{
+int get_size(YAML::Node node)
+{
+    if(auto n = node["max_pairs"])
+    {
+        return n.as<int>();
+    }
+
+    if(auto n = node["pairs"])
+    {
+        return n.size();
+    }
+
+    return default_max_pairs;
+}
+}
+
+CollisionTaskImpl::CollisionTaskImpl(YAML::Node node,
+                                     Context::ConstPtr context):
+    TaskDescriptionImpl(node, context, "collision_avoidance", get_size(node)),
+    _bound_scaling(1.0),
+    _min_dist(0.0)
+{
+    if(auto n = node["pairs"])
+    {
+        for(auto p : n)
+        {
+            _pairs.push_back(p.as<std::pair<std::string, std::string>>());
+        }
+    }
+
+    if(auto n = node["bound_scaling"])
+    {
+        _bound_scaling = n.as<double>();
+    }
+
+    if(auto n = node["distance_threshold"])
+    {
+        _min_dist = n.as<double>();
+    }
+}
+
+bool CollisionTaskImpl::validate()
+{
+    return _bound_scaling <= 1.0 &&
+            _bound_scaling > 0 &&
+            _min_dist >= 0.0;
+}
+
+double CollisionTaskImpl::getBoundScaling() const
+{
+    return _bound_scaling;
+}
+
+double CollisionTaskImpl::getDistanceThreshold() const
+{
+    return _min_dist;
+}
+
+std::list<std::pair<std::string, std::string> > CollisionTaskImpl::getWhiteList() const
+{
+    return _pairs;
+}
+
+OpenSotCollisionAdapter::OpenSotCollisionAdapter(TaskDescription::Ptr ci_task,
+                                                 Context::ConstPtr context):
+    OpenSotTaskAdapter(ci_task, context)
+{
+    _ci_coll = std::dynamic_pointer_cast<CollisionTaskImpl>(ci_task);
+    if(!_ci_coll) throw std::runtime_error("Provided task description "
+                                           "does not have expected type 'CollisionTask'");
+}
+
+OpenSoT::OptvarHelper::VariableVector OpenSotCollisionAdapter::getRequiredVariables() const
+{
+    return {};
+}
+
+TaskPtr OpenSotCollisionAdapter::constructTask()
+{
+    Eigen::VectorXd q;
+    _model->getJointPosition(q);
+
+    _opensot_coll = boost::make_shared<CollisionTaskSoT>(q,
+                                                         *_model,
+                                                         _ci_coll->getSize());
+
+    _opensot_coll->setWhiteList(_ci_coll->getWhiteList());
+
+    return _opensot_coll;
+}
+
+void OpenSotCollisionAdapter::update(double time, double period)
+{
+    OpenSotTaskAdapter::update(time, period);
+}
+
+void OpenSotCollisionAdapter::processSolution(const Eigen::VectorXd &solution)
+{
+
+}
+
+OpenSotCollisionConstraintAdapter::OpenSotCollisionConstraintAdapter(ConstraintDescription::Ptr ci_task,
+                                                                     Context::ConstPtr context):
+    OpenSotConstraintAdapter(ci_task, context)
+{
+    _ci_coll = std::dynamic_pointer_cast<CollisionTaskImpl>(ci_task);
+    if(!_ci_coll) throw std::runtime_error("Provided task description "
+                                           "does not have expected type 'CollisionTask'");
+}
+
+OpenSoT::OptvarHelper::VariableVector OpenSotCollisionConstraintAdapter::getRequiredVariables() const
+{
+    return {};
+}
+
+ConstraintPtr OpenSotCollisionConstraintAdapter::constructConstraint()
+{
+    Eigen::VectorXd q;
+    _model->getJointPosition(q);
+
+    _opensot_coll = boost::make_shared<CollisionConstrSoT>(q,
+                                                           *_model,
+                                                           0.05, // hardcoded detection th
+                                                           _ci_coll->getDistanceThreshold(),
+                                                           _ci_coll->getBoundScaling(),
+                                                           _ci_coll->getSize()
+                                                           );
+
+    _opensot_coll->setCollisionWhiteList(_ci_coll->getWhiteList());
+
+    return _opensot_coll;
+}
+
+void OpenSotCollisionConstraintAdapter::update(double time, double period)
+{
+    OpenSotConstraintAdapter::update(time, period);
+}
+
+void OpenSotCollisionConstraintAdapter::processSolution(const Eigen::VectorXd &solution)
+{
+
+}
+
+CARTESIO_REGISTER_TASK_PLUGIN(CollisionTaskImpl, CollisionConstraint)
+CARTESIO_REGISTER_TASK_PLUGIN(CollisionTaskImpl, CollisionTask)
+CARTESIO_REGISTER_OPENSOT_TASK_PLUGIN(OpenSotCollisionAdapter, CollisionTask)
+CARTESIO_REGISTER_OPENSOT_CONSTR_PLUGIN(OpenSotCollisionConstraintAdapter, CollisionConstraint)
---
+++ src/Collision.h
@@ -0,0 +1,98 @@
+#ifndef COLLISION_H
+#define COLLISION_H
+
+#include <cartesian_interface/sdk/problem/Task.h>
+#include <cartesian_interface/sdk/opensot/OpenSotTask.h>
+#include <OpenSoT/tasks/velocity/CollisionRepulsiveField.h>
+#include <OpenSoT/constraints/velocity/SelfCollisionAvoidance.h>
+
+using CollisionTaskSoT = OpenSoT::tasks::velocity::CollisionRepulsiveField;
+using CollisionConstrSoT = OpenSoT::constraints::velocity::SelfCollisionAvoidance;
+
+
+namespace XBot { namespace Cartesian { namespace collision {
+
+class CollisionTaskImpl : public virtual ConstraintDescription,
+                          public TaskDescriptionImpl
+{
+
+public:
+
+    CARTESIO_DECLARE_SMART_PTR(CollisionTaskImpl);
+
+    CollisionTaskImpl(YAML::Node node, Context::ConstPtr context);
+
+    bool validate() override;
+
+    double getBoundScaling() const;
+
+    double getDistanceThreshold() const;
+
+    std::list<std::pair<std::string, std::string>> getWhiteList() const;
+
+private:
+
+    std::list<std::pair<std::string, std::string>> _pairs;
+    double _bound_scaling;
+    double _min_dist;
+
+};
+
+class OpenSotCollisionAdapter :
+        public OpenSotTaskAdapter
+{
+
+public:
+
+    OpenSotCollisionAdapter(TaskDescription::Ptr ci_task,
+                            Context::ConstPtr context);
+
+    OpenSoT::OptvarHelper::VariableVector getRequiredVariables() const override;
+
+    virtual TaskPtr constructTask() override;
+
+    virtual void update(double time, double period) override;
+
+    virtual void processSolution(const Eigen::VectorXd& solution) override;
+
+protected:
+
+    CollisionTaskSoT::Ptr _opensot_coll;
+
+private:
+
+    CollisionTaskImpl::Ptr _ci_coll;
+    Eigen::VectorXd _x;
+
+};
+
+class OpenSotCollisionConstraintAdapter :
+        public OpenSotConstraintAdapter
+{
+
+public:
+
+    OpenSotCollisionConstraintAdapter(ConstraintDescription::Ptr ci_task,
+                                      Context::ConstPtr context);
+
+    OpenSoT::OptvarHelper::VariableVector getRequiredVariables() const override;
+
+    virtual ConstraintPtr constructConstraint() override;
+
+    virtual void update(double time, double period) override;
+
+    virtual void processSolution(const Eigen::VectorXd& solution) override;
+
+protected:
+
+    CollisionConstrSoT::Ptr _opensot_coll;
+
+private:
+
+    CollisionTaskImpl::Ptr _ci_coll;
+    Eigen::VectorXd _x;
+
+};
+
+}}}
+#endif // COLLISION_H
